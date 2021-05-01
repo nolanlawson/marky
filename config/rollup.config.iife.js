37a30e0681b9ab1f6b468deffdb6079cb31bd12e
Nolan Lawson <nolan@nolanlawson.com>
2021-05-01 12:14:09 -0700

    fix: remove __esModule for IIFE output (#24)

--- config/rollup.config.iife.js
+++ config/rollup.config.iife.js
@@ -3,5 +3,6 @@ import config from './rollup.config'
 export default config({
   format: 'iife',
   dest: 'dist/marky.js',
-  browser: true
+  browser: true,
+  skipEsModule: true // don't add __esModule for IIFE output to maintain backwards compat with marky <=v1.2.1
 })
