99e6ccf792919a42de8fa9f113bb4ad69385fe5b
Nolan Lawson <nolan@nolanlawson.com>
2022-02-15 08:13:55 -0800

    perf: fix Node 16 perf degradation (#31)
    Fixes #29

--- src/performance.js
+++ src/performance.js
@@ -1,3 +1,5 @@
 /* global performance */
-export default typeof performance !== 'undefined' && performance
 
+// TODO: Node's built-in performance API has poor performance for getEntriesByName()
+// so currently we avoid it: https://github.com/nolanlawson/marky/issues/29
+export default process.browser && typeof performance !== 'undefined' && performance
