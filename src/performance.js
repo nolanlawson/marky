6da9f2b8662edd8a4e579ffbb034fdd3b418ad17
Nolan Lawson <nolan@nolanlawson.com>
2022-02-16 06:46:21 -0800

    fix: use node perf_hooks and return value

--- src/performance.js
+++ src/performance.js
@@ -1,5 +1,13 @@
 /* global performance */
 
-// TODO: Node's built-in performance API has poor performance for getEntriesByName()
-// so currently we avoid it: https://github.com/nolanlawson/marky/issues/29
-export default process.browser && typeof performance !== 'undefined' && performance
+function safeRequire (mod) {
+  try {
+    return require(mod)
+  } catch (err) {
+    // ignore
+  }
+}
+
+export default process.browser
+  ? (typeof performance !== 'undefined' && performance)
+  : safeRequire('perf_hooks')
