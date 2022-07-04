4933c46e0f7188371d4707ff2b8202622028781d
Nolan Lawson <nolan@nolanlawson.com>
2022-07-04 11:06:58 -0700

    fix: use native performance object in Node 18 (#38)
    Fixes #17

--- src/index.js
+++ src/index.js
@@ -31,9 +31,19 @@ let clear
 if (
   perf &&
   perf.mark &&
+  perf.measure &&
   perf.getEntriesByName &&
   perf.getEntriesByType &&
-  perf.clearMeasures
+  perf.clearMarks &&
+  perf.clearMeasures &&
+  // In Node, we want to detect that this perf/correctness fix [1] is available, which
+  // landed in Node 16.15.0, 17.6.0, and 18.0.0. However, it's not observable, and
+  // we don't want to rely on fragile version checks.
+  // So we can rely on this observable change [2] to add clearResourceTimings, which
+  // landed a bit later (18.2.0), but is close enough for our purposes.
+  // [1]: https://github.com/nodejs/node/pull/42032
+  // [2]: https://github.com/nodejs/node/pull/42725
+  (process.browser || perf.clearResourceTimings)
 ) {
   mark = name => {
     throwIfEmpty(name)
--- src/performance.js
+++ src/performance.js
@@ -1,5 +1,2 @@
 /* global performance */
-
-// TODO: Node's built-in performance API has poor performance for getEntriesByName()
-// so currently we avoid it: https://github.com/nolanlawson/marky/issues/29
-export default process.browser && typeof performance !== 'undefined' && performance
+export default typeof performance !== 'undefined' && performance
