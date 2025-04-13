c6e95a5d3fe582ece8512f5c193c1288e138b55a
Nolan Lawson <nolan@nolanlawson.com>
2025-04-13 12:41:16 -0700

    feat: add typescript types

---
+++ marky.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable */
+
+export declare const mark: (name: string) => void
+
+export declare const stop: (name: string) => PerformanceEntry
+
+export declare const getEntries: () => PerformanceEntry[]
+
+export declare const clear: () => void
